1d60a96ac2c63ea34238b660db7ac002919201d6
ThisumiSamarasekara <thisumisamarasekara10@gmail.com>
2023-11-24 16:12:58 +0530

    Updating App.js & App.css

--- src/App.js
+++ src/App.js
@@ -13,46 +13,48 @@ import PlayIcon6 from "../src/images/icon-self-care.svg";
 function App() {
   return (
     <div className="App">
-      <div className="profile">
-        <Profile />
-      </div>
-      <div className="set1">
-        <div className="activity">
-          <div className="set1">
-            {data.map((item, index) => {
-              let image;
-              let backgroundColor;
-              if (index % 6 === 0) {
-                image = PlayIcon1;
-              } else if (index % 6 === 1) {
-                image = PlayIcon2;
-                backgroundColor = "hsl(195, 74%, 62%)";
-              } else if (index % 6 === 2) {
-                image = PlayIcon3;
-                backgroundColor = "hsl(348, 100%, 68%)";
-              } else if (index % 6 === 3) {
-                image = PlayIcon4;
-                backgroundColor = "hsl(145, 58%, 55%)";
-              } else if (index % 6 === 4) {
-                image = PlayIcon5;
-                backgroundColor = "hsl(264, 64%, 52%)";
-              } else if (index % 6 === 5) {
-                image = PlayIcon6;
-                backgroundColor = "hsl(43, 84%, 65%)";
-              }
+      <div className="profile_activities">
+        <div className="profile">
+          <Profile />
+        </div>
+        <div className="set1">
+          <div className="activity">
+            <div className="set1">
+              {data.map((item, index) => {
+                let image;
+                let backgroundColor;
+                if (index % 6 === 0) {
+                  image = PlayIcon1;
+                } else if (index % 6 === 1) {
+                  image = PlayIcon2;
+                  backgroundColor = "hsl(195, 74%, 62%)";
+                } else if (index % 6 === 2) {
+                  image = PlayIcon3;
+                  backgroundColor = "hsl(348, 100%, 68%)";
+                } else if (index % 6 === 3) {
+                  image = PlayIcon4;
+                  backgroundColor = "hsl(145, 58%, 55%)";
+                } else if (index % 6 === 4) {
+                  image = PlayIcon5;
+                  backgroundColor = "hsl(264, 64%, 52%)";
+                } else if (index % 6 === 5) {
+                  image = PlayIcon6;
+                  backgroundColor = "hsl(43, 84%, 65%)";
+                }
 
-              return (
-                <div key={index} className="activities">
-                  <Activity
-                    activity={item.title}
-                    image={image}
-                    hours={item.timeframes.daily.current}
-                    lastWeekHours={item.timeframes.daily.previous}
-                    backgroundColor={backgroundColor}
-                  />
-                </div>
-              );
-            })}
+                return (
+                  <div key={index} className="activities">
+                    <Activity
+                      activity={item.title}
+                      image={image}
+                      hours={item.timeframes.daily.current}
+                      lastWeekHours={item.timeframes.daily.previous}
+                      backgroundColor={backgroundColor}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
